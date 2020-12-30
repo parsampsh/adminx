caf678b016a048eb0c26b32a8682a3e544a09b3f
parsa mpsh <parsampsh@gmail.com>
2020-12-30 18:50:08 +0330

    update copyright years

--- tests/PermissionTest.php
+++ tests/PermissionTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Adminx.
  *
- * Copyright 2020 parsa shahmaleki <parsampsh@gmail.com>
+ * Copyright 2020-2021 parsa shahmaleki <parsampsh@gmail.com>
  *
  * Adminx project is Licensed Under GPL-v3.
  * For more information, please see the LICENSE file.
