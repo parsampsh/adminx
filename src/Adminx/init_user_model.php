7651cddffaec53d2ab050a89784bb593d765f94d
parsa mpsh <parsampsh@gmail.com>
2021-01-18 15:29:33 +0330

    add a comment for src/Adminx/init_user_model.php

--- src/Adminx/init_user_model.php
+++ src/Adminx/init_user_model.php
@@ -9,6 +9,13 @@
  * For more information, please see the LICENSE file.
  */
 
+// in laravel 8, models are moved from app/ to app/models
+// but some of laravel apps that migrated from 7 to 8,
+// maybe don't moved models to app/models/
+// location of models is not important for adminx
+// but as exception, location of model User is.
+// this code checks if user model is App\User,
+// makes a alias for it in App\Models\User
 if (! class_exists('\App\Models\User')) {
     if (class_exists('\App\User')){
         class_alias('App\User', 'App\Models\User');
