5510d79df7662087eccd381416aaa9e88342842a
parsa mpsh <parsampsh@gmail.com>
2021-01-20 10:33:03 +0330

    public assets: refactor font awesome from styles.css

--- src/Adminx/Views/adminx/layouts/default.blade.php
+++ src/Adminx/Views/adminx/layouts/default.blade.php
@@ -9,9 +9,8 @@ $logout_url = url($core->get_logout());
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
   <title>{{ $core->get_title() }} - @yield('adminx_title')</title>
+  <link href="{{ url('/adminx-public/default/font-awesome.css') }}" rel="stylesheet" type="text/css">
   <link href="{{ url('/adminx-public/default/styles.css') }}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   @if($core->is_rtl())
